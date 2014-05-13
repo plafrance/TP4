e0c5d31aa87f4b41c889c257fb0600fed6f93695
EmileBrunelle <emilebrunelle@icloud.com>
2014-05-13 14:23:37 -0400

    Modification de armure.py

--- source/armure.py
+++ source/armure.py
@@ -8,8 +8,9 @@
 #               armure.py                                       #
 #                                                               #
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+    
 
-
+class Armure :
     '''
     Classe Armure qui est l'armure du guerrier
 
@@ -17,26 +18,21 @@
 
     '''
 
-class Armure :
-
     @property
     def classe(self) :
         return self._classe
 
     @property
     def usure(self) :
-    '''
-    Usure de l'armure en pourcentage
+        '''
+        Usure de l'armure en pourcentage
 
-    Retourne l'usure de l'armure
-    '''
+        Retourne l'usure de l'armure
+        '''
         return self._usure
 
     @property
     def bonus(self) :
-    '''
-   Retourne bonus 0 
-    '''       
         return 0
 
 # --------------------- #
@@ -50,19 +46,19 @@ class Armure :
         self._usure = 0
 
     def __str__(self) :
-    '''
-    Sert à afficher le nom de l'armure
+        '''
+        Sert à afficher le nom de l'armure
 
-    Retour en string du nom de l'armure
-    '''
+        Retour en string du nom de l'armure
+        '''
         
         return "Armure" + str(self._classe)
 
     @usure.setter
     def usure(self, ajustement) :
-    '''
-    Retour ajustement de l'usure
-    '''
+        '''
+        Retour ajustement de l'usure
+        '''
         self._usure += ajustement
 
 if __name__ == "__main__":
