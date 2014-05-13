01dfcc71a8aa58eba4cf0d60b0b32de72bf902ce
EmileBrunelle <emilebrunelle@icloud.com>
2014-05-13 13:33:29 -0400

    Ajouter armure.py

---
+++ source/armure.py
@@ -0,0 +1,70 @@
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+#                                                               #
+#               Émile Brunelle                                  #
+#               Début : 13 mai 2014                             #
+#               Dernière modification : 13 mai 2014             #
+#               Classe Armure                                   #
+#               utf-8                                           #
+#               armure.py                                       #
+#                                                               #
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+'''
+    Classe Armure qui est l'armure du guerrier
+
+    Contient la classe, l'usure, le bonus, l'ajustement de l'usure et le nom de l'armure
+
+'''
+
+class Armure :
+
+    @property
+    def classe(self) :
+        return self._classe
+
+    @property
+    def usure(self) :
+'''
+    Usure de l'armure en pourcentage
+
+    Retourne l'usure de l'armure
+'''
+        return self._usure
+
+    @property
+    def bonus(self) :
+'''
+   Retourne bonus 0 
+'''       
+        return 0
+
+# --------------------- #
+#                       #
+#         init          #
+#                       #
+# --------------------- #
+
+    def __init__(self, une_classe) :
+        self._classe = une_classe
+        self._usure = 0
+
+    def __str__(self) :
+'''
+    Sert à afficher le nom de l'armure
+
+    Retour en string du nom de l'armure
+'''
+        
+        return "Armure" + str(self._classe)
+
+    @usure.setter
+    def usure(self, ajustement) :
+'''
+
+'''
+        self._usure += ajustement
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
