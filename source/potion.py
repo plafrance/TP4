2089481288bff14b36e5dccbc1f79ee6780e6629
Medard Jonathas <mjonathas@hotmail.com>
2014-05-15 12:53:53 -0400

    Ajout de la classe Potion

---
+++ source/potion.py
@@ -0,0 +1,54 @@
+# coding : utf-8
+#potion.py
+#Médard Jonathas
+#
+#Module pour la classe potion
+
+import item
+class Potion(item.Item):
+    """
+    Classe abstraite représentant une potion buvable par un Belligérant.
+
+        Propriété:
+            parts: Le nombre de parts que contient la fiole de potions.
+            
+    """
+
+    def __init__(self, nb_part):
+        """
+        Initialise la potion avec son nombre de parts.
+
+        Paramètre:
+            nb_part: nombre de parts initial de la potion.
+            
+        """
+        self._parts= nb_parts
+        assert self._parts > 1, "Le nombre de part ne peut être plus petit que 1"
+
+    @property
+    def parts(self):
+        return self._parts
+
+    def faire_boire(self, cible):
+        """
+        Fait boire la potion par un belligérant
+
+        Paramètre:
+            cible: Belligérant qui va boire la potion
+        
+        """
+        
+        self._parts -= 1
+        self.activer(cible)
+
+    def activer(self, cible):
+        """
+        Méthode abstraite qui exécute l'effet de la potion sur la cible
+        
+        """
+        assert self._parts > 1, "Le nombre de part ne peut être plus petit que 1"
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
