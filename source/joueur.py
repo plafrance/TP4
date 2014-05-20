82f15c0947c2a98e6672546d7a987e82d87107a2
Andrew Caron <andrew.caron12@gmail.com>
2014-05-20 08:57:24 -0400

    Ajout de doc string(non terminer)

--- source/joueur.py
+++ source/joueur.py
@@ -10,25 +10,69 @@ import équipe
 import action
 
 class Joueur:
+    """
+    Un joueur
+
+    Attributs :
+         _contrôle : Contrôle , Contient un contrôle
+         
+    Paramètres :
+        _nom : str, le nom du joueur
+        _équipe : Équipe, L'équipe que contrôle le joueur
+        _actions_par_tour L int, Le nombre d'action que le joueur peut faire
+    """
     def __init__( self , un_nom , un_contrôle ):
+        """
+        Initialise le Joueur avec un nom sans espaces au début et à la fin et un Contrôle.
+
+        Paramètres :
+           _un_nom : str, le pseudonyme du Joueur
+           _un_contrôle: Contrôle, Le Contrôle par lequel le joueur interagira avec le jeu.
+           
+        """
         self._nom = un_nom.strip()
         self._contrôle = un_contrôle
         assert( self._nom == '' or self._nom == None) , " nom invalide "
 
     def __str__( self ):
+        """
+        Donne une représentation en chaîne de caractères du Joueur
+
+        Retour : str, Le nom du joueur
+        """
         return str(self._nom)
 
     def choisir_belligérant( self ):
+        """
+        Utilise le contrôle pour permettre au joueur de choisir un belligérant dans sa propre équipe.
+
+        Retour : Belligérant, Le belligérant selectionné.
+        """
         choisir_cible(self._équipe)
 
     def choisir_action( self , un_acteur ):
+        """
+        Utilise le contrôle pour permettre au joueur de choisir sa prochaine action.
 
+        Paramètres :
+            un_acteur : Belligérant, Le belligérant qui doit faire l'action
+
+        Retour : Action, L'action choisie par le joueur
+        """
         choix = self._contrôle.choisir([action for action in enumerate(Action)])
 
         if not isinstance( un_acteur , Mage ):
             choix.delete(Action.JETER_SORT)
         
     def choisir_cible( self , _équipe ):
+        """
+        Utilise le contrôle pour permettre au joueur de choisir un belligérant ciblé par une action. Le belligérant peut être de son équipe ou de celle d'un adversaire.
+
+        Paramètres ;
+            les équipes : Liste d'Équipe, La liste de toutes les équipes parmi lesquelles le joueur peut choisir sa cible.
+
+        Retour : Belligérant, Le belligérant sélectionné.
+        """
         liste_belligérant = []
         liste_belligérant_possible = []
 
@@ -42,9 +86,25 @@ class Joueur:
         return contrôle.choisir_objet(liste_belligérant_possible)
 
     def créer_belligérant(self):
-        return belligérant.Belligérant(contrôle.saisir("Choissez le nom de votre belligérant"))
+        """
+        Crée un belligérant et l'ajoute à l'équipe du jouer.
+
+        Retour : None
+        """
+        _équipe.add(belligérant.Belligérant(contrôle.saisir("Choissez le nom de votre belligérant")))
 
     def jouer(self):
+        """
+        Utilise le contrôle pour permettre au joueur de jouer son tour.
+        Un tour comporte les étapes suivantes :
+        1.Choisir un belligérant dans son équipe.
+        2.Choisir une action que ce belligérant doit effectuer.
+        3.Si l'action est un attaque, choisir son adversaire.
+        4.Si l'action concerne un item, choisir l'item.
+        5.Si l'action est jeter un sort, choisir le sort et la cible.
+        6.Effectuer l'action sur la cible.
+        
+        """
         mon_belligérant = choisir_belligérant()
         action = choisir_action(mon_belligérant)
         if action == Action.ATTAQUER:
